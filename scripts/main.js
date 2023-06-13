1fbd7dffe89fbcb07313a7dfd01c0d5a9c971c50
Tom Shaver <l3l_aze@yahoo.com>
2023-06-12 22:47:15 -0500

    Integrating CoC Assistant
    * Patched in CoC Assistant code from other project.
    * Added Mode button to switch between CB and CoC.
    * Added Reset button for CoC.
    * Known Bug: At mobile sizes (600px or less), the CoC Reset button can't be `display: none` while CoC is active and the menu is open, so is just `visibility: hidden`.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+'use strict'
+
 document.getElementById('hamburger').addEventListener('click', function () {
   document.getElementById('controls').classList.toggle('responsive')
   document.getElementById('hamburger').classList.toggle('active')
@@ -18,14 +20,6 @@ document.getElementById('modal_container').addEventListener('click', function (e
 document.getElementById('closeModal').addEventListener('click', function () {
   document.getElementById('overlay_container').style.display = 'none'
 })
-function reset () {
-  let node
-
-  for (let i = 0; i < 25; i++) {
-    node = document.getElementById('node' + i)
-    node.classList.remove('hit')
-  }
-}
 
 function openModal (which) {
   const modals = [
@@ -48,4 +42,33 @@ function openModal (which) {
 
   document.getElementById('controls').classList.toggle('responsive')
   document.getElementById('hamburger').classList.toggle('active')
+}
+
+function switchMode () {
+  const cbc = document.getElementById('cb_container')
+  const ccc = document.getElementById('coc_container')
+
+  const modeBtn = document.getElementById('modeBtn')
+  const mode = modeBtn.innerText.split(' ')[1]
+
+  document.getElementById('controls').classList.remove('responsive')
+  document.getElementById('hamburger').classList.remove('active')
+
+  if (mode === 'CB') {
+    ccc.style.display = 'block'
+    cbc.style.display = 'none'
+    modeBtn.innerText = 'Mode: CoC'
+
+    document.getElementById('auto').style.display = 'none'
+    document.getElementById('next').style.display = 'none'
+    document.getElementById('reset').style.display = 'block'
+  } else {
+    ccc.style.display = 'none'
+    cbc.style.display = 'block'
+    modeBtn.innerText = 'Mode: CB'
+
+    document.getElementById('auto').style.display = ''
+    document.getElementById('next').style.display = Array.from(document.querySelectorAll('.row')).length > 0 ? '' : 'none'
+    document.getElementById('reset').style.display = 'none'
+  }
 }
