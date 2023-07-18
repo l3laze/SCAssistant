7ea77e7a53a5f23312ae279c0c2081fddd5910f9
Tom Shaver <l3l_aze@yahoo.com>
2023-07-18 03:21:47 -0500

    CA Optimizations
    * Removed over-self-explaining garbage, greatly streamlined what little there is.

--- scripts/clash-assistant.js
+++ scripts/clash-assistant.js
@@ -2,19 +2,14 @@ const clashAssistant = (function () { /* eslint-disable-line no-unused-vars */
   'use strict'
 
   document.getElementById('coc-container').addEventListener('click', function clickClash (event) { /* eslint-disable-line no-unused-vars */
-    const targetElementId = event.target.id
-
-    if (/node-\d{1,2}/.test(targetElementId)) {
-      document.getElementById(targetElementId).classList.toggle('hit')
+    if (/node-\d{1,2}/.test(event.target.id)) {
+      event.target.classList.toggle('hit')
     }
   })
 
   function resetClashBoard () { /* eslint-disable-line no-unused-vars */
-    let node
-
     for (let i = 0; i < 25; i++) {
-      node = document.getElementById('node-' + i)
-      node.classList.remove('hit')
+      document.getElementById('node-' + i).classList.remove('hit')
     }
   }
 
